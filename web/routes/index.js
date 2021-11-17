0edb2b14b3b471c0a12f60f6c79762e2ee340251
Andreas Lacza <andreas.lacza@me.com>
2021-11-17 12:39:53 +0100

    Update index.js

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,11 +1,22 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {
-    title: 'SmartHochschule'
-  });
+/**
+ * GET home page.
+ */
+
+router.get('/', function (req, res, next) {
+
+    // render html page.
+    res.render('index', {
+        title: 'SmartHochschule',
+    });
 });
 
 module.exports = router;
